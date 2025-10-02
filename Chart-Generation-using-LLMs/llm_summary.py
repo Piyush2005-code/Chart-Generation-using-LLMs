95946b740ac3d25f45354d5eba4b4937a22c239e
Baviskar Darpan Chandrashekhar <darpanbaviskar@gmail.com>
2025-10-02 16:04:19 +0530

    Refactor API key management for Gemini model
    Updated API key handling to use environment variables.

--- Chart-Generation-using-LLMs/llm_summary.py
+++ Chart-Generation-using-LLMs/llm_summary.py
@@ -6,12 +6,17 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.documents import Document
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from dotenv import load_dotenv
 
+# Load environment variables
+load_dotenv()
 # If using environment variable for API Key
-os.environ["GOOGLE_API_KEY"] = "AIzaSyCD2LStsBJXjXNtysgp9Ma3hrCCmopRo-4"
+api_key = os.getenv("GEMINI_API_KEY")
+# If using environment variable for API Key
+
 
 # Initialize Gemini model (set to use Gemini, adjust model name as needed)
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", api_key=api_key)
 
 # The prompt template for diagram-relevant summary
 prompt = ChatPromptTemplate.from_template(
