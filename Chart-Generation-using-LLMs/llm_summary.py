e046879ebf6bfa5ca6e84f650a87d9bb27818ade
Baviskar Darpan Chandrashekhar <darpanbaviskar@gmail.com>
2025-10-02 16:07:33 +0530

    Load API key from environment variable
    Updated API key handling to use environment variables.

--- Chart-Generation-using-LLMs/llm_summary.py
+++ Chart-Generation-using-LLMs/llm_summary.py
@@ -7,11 +7,16 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.documents import Document
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 # If using environment variable for API Key
-os.environ["GOOGLE_API_KEY"] = "AIzaSyCD2LStsBJXjXNtysgp9Ma3hrCCmopRo-4"
+api_key = os.getenv("GEMINI_API_KEY")
+
 
 # Initialize Gemini model (set to use Gemini, adjust model name as needed)
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", api_key=api_key)
 
 # The prompt template for diagram-relevant summary
 prompt = ChatPromptTemplate.from_template(
